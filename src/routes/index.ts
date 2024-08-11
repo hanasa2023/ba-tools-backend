b1f91e22eaabb5c8005499ffa4a99729bc1f9ca5
hanasaki <hanasakayui2022@gmail.com>
2024-08-12 03:10:59 +0800

    🔥：删除无用文件

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,28 +1,15 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import Paths from '../common/Paths';
-import UserRoutes from './UserRoutes';
-
+import Paths from "../common/Paths";
 
 // **** Variables **** //
 
 const apiRouter = Router();
 
-
 // ** Add UserRouter ** //
 
 // Init router
 const userRouter = Router();
-
-// Get all users
-userRouter.get(Paths.Users.Get, UserRoutes.getAll);
-userRouter.post(Paths.Users.Add, UserRoutes.add);
-userRouter.put(Paths.Users.Update, UserRoutes.update);
-userRouter.delete(Paths.Users.Delete, UserRoutes.delete);
-
-// Add UserRouter
-apiRouter.use(Paths.Users.Base, userRouter);
-
 
 // **** Export default **** //
 
